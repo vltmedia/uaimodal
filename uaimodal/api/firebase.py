d96f3315f261e191e5ce44baa613cb991bdc1e61
info@vltmedia.com <info@vltmedia.com>
2024-08-22 22:03:48 -0400

    Feature: Added Firebase Feature; Added Utils

---
+++ uaimodal/api/firebase.py
@@ -0,0 +1,130 @@
+from firebase_admin import credentials, initialize_app, storage, firestore
+from uaimodal.utils import rootPath 
+cred = None
+
+db : firestore.client.Client = None
+
+
+def initFirebase(servicePath="service.json" ,bucket = "bucket.appspot.com") -> tuple[firestore.client.Client, credentials.Certificate]:
+    global db
+    global cred
+    
+    cred = credentials.Certificate(servicePath)
+    initialize_app(cred, {'storageBucket': bucket})
+    db = firestore.client()
+    return db, cred
+
+def getDB() -> firestore.client.Client:
+    global db
+    if db is None:
+        initFirebase()
+    return db
+
+def getCred():
+    global cred
+    if cred is None:
+        initFirebase()
+    return cred
+
+def upload_file_to_space(file_src, save_as, **kwargs):
+    """
+    :param spaces_client: Your DigitalOcean Spaces client from get_spaces_client()
+    :param space_name: Unique name of your space. Can be found at your digitalocean panel
+    :param file_src: File location on your disk
+    :param save_as: Where to save your file in the space
+    :param kwargs
+    :return:
+    """
+    bucket = storage.bucket()
+    blob = bucket.blob(save_as)
+    blob.upload_from_filename(file_src)
+
+    # Opt : if you want to make public access from the URL
+    blob.make_public()
+    return blob.public_url 
+
+
+def getDoc(collection, doc):
+    db = getDB()
+    doc_ref = db.collection(collection).document(doc)
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        return None
+    
+def setDoc(collection, doc, data):
+    db = getDB()
+    doc_ref = db.collection(collection).document(doc)
+    doc_ref.set(data)
+    
+def getCollection(collection):
+    db = getDB()
+    docs = db.collection(collection).stream()
+    return [doc.to_dict() for doc in docs]
+
+def deleteDoc(collection, doc):
+    db = getDB()
+    doc_ref = db.collection(collection).document(doc)
+    doc_ref.delete()
+    
+def getStorageURL(path):
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    return blob.public_url
+
+def getStorageBytes(path):
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    return blob.download_as_bytes()
+
+def getStorageText(path):
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    return blob.download_as_string()
+
+def getStorageJson(path):
+    import json
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    return json.loads(blob.download_as_string())
+
+def saveStringToStorage(data, path, public=True):
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    blob.upload_from_string(data)
+    if public:
+        blob.make_public()
+    
+def saveFileObjectToStorage(fileObject, path, public=True):
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    blob.upload_from_file(fileObject)
+    if public:
+        blob.make_public()
+        
+def saveBytesToStorage(data, path, public=True):
+    from uaimodal.utils import BytesToBase64
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    blob.upload_from_string(BytesToBase64(data))
+    if public:
+        blob.make_public()
+    
+def saveJsonToStorage(data, path, public=True):
+    import json
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    blob.upload_from_string(json.dumps(data))
+    if public:
+        blob.make_public()
+        
+def deleteStorage(path):
+    bucket = storage.bucket()
+    blob = bucket.blob(path)
+    blob.delete()
+    
+def getStorageBlob(path):
+    bucket = storage.bucket()
+    return bucket.blob(path)
+
---
+++ uaimodal/utils.py
@@ -0,0 +1,59 @@
+
+import os, sys
+
+rootPath = "/root"
+
+pythonPath = "python"
+
+projectDir = f"{rootPath}"
+
+def getRootPath(defaultPath = "/root"):
+    if not os.path.exists(defaultPath):
+        defaultPath = os.path.dirname(__file__)
+    return defaultPath
+
+
+
+def make_archive(source, destination):
+    import shutil
+    base_name = '.'.join(destination.split('.')[:-1])
+    format = destination.split('.')[-1]
+    root_dir = os.path.dirname(source)
+    base_dir = os.path.basename(source.strip(os.sep))
+    shutil.make_archive(base_name, format, root_dir, base_dir)
+
+def GetURLBytes(url):
+    import requests
+    r = requests.get(url)
+    return r.content
+
+def GetURLText(url):
+    import requests
+    r = requests.get(url)
+    return r.text
+
+def GetURLJson(url):
+    import requests
+    r = requests.get(url)
+    return r.json()
+
+def BytesToBase64(data):
+    import base64
+    return base64.b64encode(data).decode()
+
+def Base64ToBytes(data):
+    import base64
+    return base64.b64decode(data)
+
+def SaveToPath(data, path):
+    with open(path, "wb") as f:
+        f.write(data)
+        
+def ReadFromPath(path):
+    with open(path, "rb") as f:
+        return f.read()
+    
+def ReadFromPathBase64(path):
+    with open(path, "rb") as f:
+        return BytesToBase64(f.read())
+    
