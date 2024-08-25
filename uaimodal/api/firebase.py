0282ab10aba6e07f0ebe205b91fdaaeccb8b7748
info@vltmedia.com <info@vltmedia.com>
2024-08-24 22:21:53 -0400

    Feature: Added Set Result Feature: Added job docs

--- uaimodal/api/firebase.py
+++ uaimodal/api/firebase.py
@@ -6,6 +6,17 @@ db  = None
 
 
 def initFirebase(servicePath="service.json" ,bucket = "bucket.appspot.com") :
+    """
+    Initializes the Firebase connection and returns the Firestore client and credentials.
+
+    Args:
+        servicePath (str, optional): The path to the service account JSON file. Defaults to "service.json".
+        bucket (str, optional): The storage bucket URL. Defaults to "bucket.appspot.com".
+
+    Returns:
+        db (google.cloud.firestore.Client): The Firestore client.
+        cred (google.auth.credentials.Certificate): The Firebase credentials.
+    """
     global db
     global cred
     
@@ -15,12 +26,30 @@ def initFirebase(servicePath="service.json" ,bucket = "bucket.appspot.com") :
     return db, cred
 
 def getDB() :
+    """
+    Retrieves the Firebase database instance.
+
+    If the database instance is not initialized, it will be initialized by calling the `initFirebase` function.
+
+    Returns:
+        The Firebase database instance.
+
+    """
     global db
     if db is None:
         initFirebase()
     return db
 
 def getCred():
+    """
+    Retrieves the Firebase credentials.
+
+    If the credentials have not been initialized, this function calls the `initFirebase` function to initialize them.
+
+    Returns:
+        The Firebase credentials.
+
+    """
     global cred
     if cred is None:
         initFirebase()
@@ -46,8 +75,10 @@ def upload_file_to_space(file_src, save_as, **kwargs):
 def initDoc(collection) -> str:
     """
     Generate a new document in the collection and return the document ID. This is useful so you don't have to create the document id and possibly have duplicates.
+
     Args:
-        str: The name of the collection where the document will be created.
+        collection (str): The name of the collection where the document will be created.
+
     Returns:
         str: The ID of the newly created document.
     """
@@ -56,7 +87,17 @@ def initDoc(collection) -> str:
     docID = doc_ref.add({"name":""}).id
     return docID
 
-def getDoc(collection, doc) ->dict:
+def getDoc(collection, doc) -> dict:
+    """
+    Retrieves a document from a specified collection in the Firebase database.
+
+    Args:
+        collection (str): The name of the collection to retrieve the document from.
+        doc (str): The ID of the document to retrieve.
+
+    Returns:
+        dict: A dictionary representing the retrieved document, or None if the document does not exist.
+    """
     db = getDB()
     doc_ref = db.collection(collection).document(doc)
     doc = doc_ref.get()
@@ -66,6 +107,18 @@ def getDoc(collection, doc) ->dict:
         return None
     
 def setDoc(collection, doc, data) -> dict:
+    """
+    Sets the data for a document in a collection in the Firebase Firestore database.
+
+    Args:
+        collection (str): The name of the collection in which the document resides.
+        doc (str): The ID of the document to be updated.
+        data (dict): The data to be set for the document.
+
+    Returns:
+        dict: The updated document as a dictionary.
+
+    """
     db = getDB()
     doc_ref = db.collection(collection).document(doc)
     doc_ref.set(data)
@@ -78,32 +131,100 @@ def getCollection(collection):
     return [doc.to_dict() for doc in docs]
 
 def deleteDoc(collection, doc):
+    """
+    Deletes a document from a specified collection in the Firebase Firestore database.
+
+    Args:
+        collection (str): The name of the collection where the document is located.
+        doc (str): The ID of the document to be deleted.
+
+    Returns:
+        None
+    """
     db = getDB()
     doc_ref = db.collection(collection).document(doc)
     doc_ref.delete()
     
 def getStorageURL(path):
+    """
+    Retrieves the public URL of a file stored in the Firebase storage.
+
+    Args:
+        path (str): The path of the file in the storage.
+
+    Returns:
+        str: The public URL of the file.
+
+    """
     bucket = storage.bucket()
     blob = bucket.blob(path)
     return blob.public_url
 
 def getStorageBytes(path):
+    """
+    Retrieves the bytes of a file from the storage bucket.
+
+    Args:
+        path (str): The path to the file in the storage bucket.
+
+    Returns:
+        bytes: The bytes of the file.
+
+    """
     bucket = storage.bucket()
     blob = bucket.blob(path)
     return blob.download_as_bytes()
 
 def getStorageText(path):
+    """
+    Retrieves the text content of a file from a storage bucket.
+
+    Args:
+        path (str): The path to the file in the storage bucket.
+
+    Returns:
+        str: The text content of the file.
+
+    """
     bucket = storage.bucket()
     blob = bucket.blob(path)
     return blob.download_as_string()
 
 def getStorageJson(path):
+    """
+    Retrieves a JSON file from a storage bucket.
+
+    Args:
+        path (str): The path to the JSON file in the storage bucket.
+
+    Returns:
+        dict: The JSON data as a dictionary.
+
+    Raises:
+        None
+
+    Example:
+        >>> getStorageJson('path/to/file.json')
+        {'key1': 'value1', 'key2': 'value2'}
+    """
     import json
     bucket = storage.bucket()
     blob = bucket.blob(path)
     return json.loads(blob.download_as_string())
 
 def saveStringToStorage(data, path, public=True):
+    """
+    Saves a string to a storage bucket.
+
+    Args:
+        data (str): The string data to be saved.
+        path (str): The path where the string data will be saved in the storage bucket.
+        public (bool, optional): Determines whether the saved file should be publicly accessible. 
+                                 Defaults to True.
+
+    Returns:
+        None
+    """
     bucket = storage.bucket()
     blob = bucket.blob(path)
     blob.upload_from_string(data)
@@ -111,6 +232,17 @@ def saveStringToStorage(data, path, public=True):
         blob.make_public()
     
 def saveFileObjectToStorage(fileObject, path, public=True):
+    """
+    Saves a file object to a storage bucket.
+
+    Args:
+        fileObject: The file object to be saved.
+        path: The path where the file should be stored in the bucket.
+        public: A boolean indicating whether the file should be made public (default is True).
+
+    Returns:
+        None
+    """
     bucket = storage.bucket()
     blob = bucket.blob(path)
     blob.upload_from_file(fileObject)
@@ -118,6 +250,17 @@ def saveFileObjectToStorage(fileObject, path, public=True):
         blob.make_public()
         
 def saveBytesToStorage(data, path, public=True):
+    """
+    Saves bytes data to a storage bucket.
+
+    Args:
+        data: The bytes data to be saved.
+        path: The path where the data will be stored in the bucket.
+        public: A boolean indicating whether the stored data should be made public (default is True).
+
+    Returns:
+        None
+    """
     from uaimodal.utils import BytesToBase64
     bucket = storage.bucket()
     blob = bucket.blob(path)
@@ -126,6 +269,18 @@ def saveBytesToStorage(data, path, public=True):
         blob.make_public()
     
 def saveJsonToStorage(data, path, public=True):
+    """
+    Saves a JSON object to a storage bucket.
+
+    Args:
+        data (dict): The JSON object to be saved.
+        path (str): The path to the storage bucket where the JSON object will be saved.
+        public (bool, optional): Specifies whether the saved JSON object should be made public. 
+                                 Defaults to True.
+
+    Returns:
+        None
+    """
     import json
     bucket = storage.bucket()
     blob = bucket.blob(path)
@@ -134,15 +289,48 @@ def saveJsonToStorage(data, path, public=True):
         blob.make_public()
         
 def deleteStorage(path):
+    """
+    Deletes a file from the Firebase storage.
+
+    Args:
+        path (str): The path of the file to be deleted.
+
+    Returns:
+        None
+    """
     bucket = storage.bucket()
     blob = bucket.blob(path)
     blob.delete()
     
 def getStorageBlob(path):
+    """
+    Retrieves a storage blob from the default bucket.
+
+    Args:
+        path (str): The path to the blob.
+
+    Returns:
+        Blob: The storage blob object.
+
+    """
     bucket = storage.bucket()
     return bucket.blob(path)
 
 def getJob(jobId, state="pending"):
+    """
+    Retrieves a job document from the specified collection based on the given job ID and state.
+
+    Parameters:
+    - jobId (str): The ID of the job to retrieve.
+    - state (str, optional): The state of the job. Defaults to "pending".
+
+    Returns:
+    - dict: The job document.
+
+    Raises:
+    - ValueError: If an invalid state is provided.
+
+    """
     if state == "pending":
         return getDoc("jobs_pending", jobId)
     elif state == "running":
@@ -151,6 +339,18 @@ def getJob(jobId, state="pending"):
         return getDoc("jobs_finished", jobId)
     
 def findJob(jobId):
+    """
+    Finds a job with the given jobId and returns the job object along with its state.
+
+    Parameters:
+        jobId (str): The ID of the job to find.
+
+    Returns:
+        tuple: A tuple containing the job object and its state. The state can be one of the following:
+            - "pending" if the job is in the pending state.
+            - "running" if the job is in the running state.
+            - "finished" if the job is in the finished state.
+    """
     state = "pending"
     job = getJob(jobId, "pending")
     if job is None:
@@ -162,6 +362,18 @@ def findJob(jobId):
     return job, state
 
 def setJob(jobId, data, state="pending"):
+    """
+    Sets the job with the given jobId to the specified state and updates the job data.
+
+    Parameters:
+    - jobId (str): The ID of the job to be updated.
+    - data (dict): The updated data for the job.
+    - state (str, optional): The state to set the job to. Defaults to "pending".
+
+    Returns:
+    None
+    """
+    
     ## set job to the correct state queue, then delete from the previous queue.
     job_, prevState = findJob(jobId)
     if job_ is not None:
@@ -169,32 +381,122 @@ def setJob(jobId, data, state="pending"):
     setDoc(f"jobs_{state}", jobId, data)
     
 def setJobPending(jobId, data):
+    """
+    Sets the job status to 'pending' in the Firebase database.
+
+    Parameters:
+    - jobId (str): The ID of the job.
+    - data (dict): The data to be set for the job.
+
+    Returns:
+    None
+    """
     setJob(jobId, data, "pending")
     
 def setJobRunning(jobId, data):
+    """
+    Sets the status of a job to 'running' in the Firebase database.
+
+    Parameters:
+    - jobId (str): The ID of the job.
+    - data (dict): The data associated with the job.
+
+    Returns:
+    None
+    """
     setJob(jobId, data, "running")
     
 def setJobFinished(jobId, data):
+    """
+    Sets the status of a job to 'finished' in the Firebase database.
+
+    Parameters:
+    - jobId (str): The ID of the job.
+    - data (dict): The data to be updated for the job.
+
+    Returns:
+    None
+    """
     setJob(jobId, data, "finished")
     
+def updateJobResult(jobId, data):
+    """
+    Updates the result of a job with the given jobId.
+
+    Args:
+        jobId (str): The ID of the job to update.
+        data (any): The result data to be assigned to the job.
+
+    Returns:
+        None
+    """
+    job_, state = findJob(jobId)
+    if job_ is not None:
+        job_["result"] = data
+        setJob(jobId, job_, "finished")
+        
+        
 def deleteJob(jobId):
+    """
+    Deletes a job from the specified state collection.
+
+    Args:
+        jobId (str): The ID of the job to be deleted.
+
+    Returns:
+        None
+    """
     job_, state = findJob(jobId)
     if job_ is not None:
         deleteDoc(f"jobs_{state}", jobId)
         
 def getPendingJobs():
+    """
+    Retrieves the collection of pending jobs from the Firebase database.
+
+    Returns:
+        list: A list of pending jobs.
+    """
     return getCollection("jobs_pending")
 
 def getRunningJobs():
+    """
+    Retrieves the collection of running jobs from the Firebase database.
+
+    Returns:
+        The collection of running jobs.
+    """
     return getCollection("jobs_running")
 
 def getFinishedJobs():
+    """
+    Retrieves the collection of finished jobs from the Firebase database.
+
+    Returns:
+        list: A list of finished jobs.
+    """
     return getCollection("jobs_finished")
 
 def getJobs():
+    """
+    Retrieves all jobs from the database.
+
+    Returns:
+        A list of all jobs, including pending, running, and finished jobs.
+    """
     return getPendingJobs() + getRunningJobs() + getFinishedJobs()
 
 def getJobResults(jobId):
+    """
+    Retrieves the results of a finished job from the 'jobs_finished' collection.
+
+    Parameters:
+    - jobId (str): The ID of the job to retrieve results for.
+
+    Returns:
+    - dict: The document representing the job results.
+
+    """
     return getDoc("jobs_finished", jobId)
 
 
